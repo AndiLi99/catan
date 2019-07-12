e27e4f8c38104d02c35d02213c8be964e92f4748
a259l <a259li@uwaterloo.ca>
2019-07-12 19:29:45 -0400

    Implemented edge relationships

--- src/edge.cc
+++ src/edge.cc
@@ -24,14 +24,60 @@ std::ostream& operator<<(std::ostream& out, const Edge& edge){
     
  */
 std::vector<Hexagon> Edge::joins(){
-    return std::vector<Hexagon>{};
+    switch(s){
+        case Side::N: return std::vector<Hexagon>{
+            Hexagon{a,b},
+            Hexagon{a,b+1}
+        };
+        case Side::E: return std::vector<Hexagon>{
+            Hexagon{a,b},
+            Hexagon{a+1,b}
+        };
+        case Side::W: return std::vector<Hexagon>{
+            Hexagon{a,b},
+            Hexagon{a-1,b+1}
+        };
+    }
 }
 
 std::vector<Edge> Edge::continues(){
-    return std::vector<Edge>{};
+    switch(s){
+        case Side::N: return std::vector<Edge>{
+            Edge{a, b, Side::W},
+            Edge{a, b, Side::E},
+            Edge{a, b-1, Side::E},
+            Edge{a+1, b, Side::W},
+        };
+        case Side::E: return std::vector<Edge>{
+            Edge{a, b, Side::N},
+            Edge{a, b+1, Side::W},
+            Edge{a, b+1, Side::N},
+            Edge{a+1, b, Side::W},
+        };
+        case Side::W: return std::vector<Edge>{
+            Edge{a, b, Side::N},
+            Edge{a-1, b, Side::E},
+            Edge{a-1, b, Side::N},
+            Edge{a, b-1, Side::E},
+        };
+    }
+    
 }
 
 std::vector<Vertex> Edge::endpoints(){
-    return std::vector<Vertex>{};
+    switch(s){
+        case Side::N: return std::vector<Vertex>{
+            Vertex{a+1,b, Corner::L},
+            Vertex{a-1,b+1, Corner::R}
+        };
+        case Side::E: return std::vector<Vertex>{
+            Vertex{a,b, Corner::R},
+            Vertex{a+1,b, Corner::L}
+        };
+        case Side::W: return std::vector<Vertex>{
+            Vertex{a,b, Corner::L},
+            Vertex{a-1,b+1, Corner::R}
+        };
+    }
 }
 
