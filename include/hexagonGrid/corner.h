6eef848d998b1d32526c5139497714329b63c74e
a259l <a259li@uwaterloo.ca>
2019-07-12 20:26:55 -0400

    Added forward declarations to enum class output operators

--- include/hexagonGrid/corner.h
+++ include/hexagonGrid/corner.h
@@ -1,5 +1,6 @@
 #ifndef CORNER_H
 #define CORNER_H
+#include <iosfwd>
 enum class Corner{L, R};
 
 std::ostream& operator<<(std::ostream& out, Corner corner);
--- include/hexagonGrid/side.h
+++ include/hexagonGrid/side.h
@@ -1,5 +1,6 @@
 #ifndef SIDE_H
 #define SIDE_H
+#include <iosfwd>
 enum class Side{W, N, E};
 
 std::ostream& operator<<(std::ostream& out, Side side);
