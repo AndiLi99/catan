5032d595c049983744b77674cf350fc1e72bd607
a259l <a259li@uwaterloo.ca>
2019-07-12 22:09:23 -0400

    Fleshing out board class

--- include/board.h
+++ include/board.h
@@ -1,13 +1,29 @@
-#ifndef BOARD
-#define BOARD
-#include "hexagonGrid.h"
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <unordered_map>
+#include "hexagon.h"
+#include "edge.h"
+#include "vertex.h"
 #include "tile.h"
 #include "connection.h"
 #include "intersection.h"
 
 class Board {
-		HexagonGrid<Tile, Connection, Intersection> hexGrid;
+		std::unordered_map<Hexagon, Tile, HexagonHash, HexagonEquals> tiles;
+        std::unordered_map<Edge, Connection, EdgeHash, EdgeEquals> connections;
+        std::unordered_map<Vertex, Intersection, VertexHash, VertexEquals> intersections;;
+		Hexagon robber;
+		void resetTiles();
+		void resetConnections();
+		void resetIntersections();
 	public:
+		void resetBoard();
+		void defaultSetup();
+		void randomSetup();
+		void addSettlement(Vertex vertex, int playerID);
+		void upgradeSettlement(Vertex vertex);
+		void addRoad(Edge edge, int playerID);
 
 };
 #endif
