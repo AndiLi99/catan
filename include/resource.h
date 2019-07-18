c25ab949b931a9fe09b98f66b1716ef80b36d4df
AndiLi99 <a259li@uwaterloo.ca>
2019-07-17 21:12:03 -0400

    Renamed resources to match text display

--- include/resource.h
+++ include/resource.h
@@ -1,7 +1,6 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 #include <iosfwd>
-enum class Resource{Wood, Brick, Wheat, Sheep, Stone};
+enum Resource{Lumber, Brick, Grain, Wool, Ore};
 
-std::ostream& operator<<(std::ostream& out, Resource resource);
 #endif
